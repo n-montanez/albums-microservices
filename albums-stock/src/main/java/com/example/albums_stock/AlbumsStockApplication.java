277032d765505948691583590d1ccbbe4b7fb00b
Nicolás Montañez Velasco <nicolas.montanez@globant.com>
2025-02-07 09:48:04 -0500

    init: albums stock service

---
+++ albums-stock/src/main/java/com/example/albums_stock/AlbumsStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.albums_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumsStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumsStockApplication.class, args);
+	}
+
+}
---
+++ albums-stock/src/test/java/com/example/albums_stock/AlbumsStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.albums_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumsStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
